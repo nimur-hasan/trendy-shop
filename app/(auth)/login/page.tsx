dc86faa6fd904ca3f0d7ca46e129696bcd323d80
nimur-hasan <developernaim20@gmail.com>
2024-07-07 16:09:52 +0600

    middleware config

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+      <div>
+          <Button>Login</Button>
+    </div>
+  )
+}
---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const pathname = request.nextUrl.pathname;
+
+  const jwt = request.cookies.get("jwt");
+
+  const publicRoutes = ["/", "/contact-us", "/privacy-policy"];
+
+  if (publicRoutes.includes(pathname)) {
+    return response;
+  }
+
+  if (!jwt && pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return response;
+}
+
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
